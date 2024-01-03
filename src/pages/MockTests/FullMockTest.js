8c931ed8dcda52e818f9a1874cd6106c55dacaf6
Afaq Ahmed <afaaaq85@gmail.com>
2024-01-03 16:55:36 +0500

    created different options in full mock test

--- src/pages/MockTests/FullMockTest.js
+++ src/pages/MockTests/FullMockTest.js
@@ -17,11 +17,44 @@ const FullMockTest = () => {
         </div>
       </div>
       <div className="create-mock-div">
-        <h2>Free Mock Test</h2>
+        <div className="mock-div-topbar">
+          <h2 className="mock-topbar-title">Free mock test 1</h2>
+          <p>1hrs format</p>
+        </div>
+        <div className="create-mock-attributes">
+          <div>
+            <p>Questions: 15-20</p>
+            <p>Approx time: 1 hours</p>
+          </div>
+          <div>
+            <Button variant="contained">Start</Button>
+          </div>
+        </div>
+      </div>
+      <div className="create-mock-div">
+        <div className="mock-div-topbar">
+          <h2 className="mock-topbar-title">Free mock test 2</h2>
+          <p>2hrs format</p>
+        </div>
         <div className="create-mock-attributes">
           <div>
             <p>Questions: 25-30</p>
-            <p>Approx time: 1 hour</p>
+            <p>Approx time: 2 hours</p>
+          </div>
+          <div>
+            <Button variant="contained">Start</Button>
+          </div>
+        </div>
+      </div>
+      <div className="create-mock-div">
+        <div className="mock-div-topbar">
+          <h2 className="mock-topbar-title">Free mock test 3</h2>
+          <p>2hrs format</p>
+        </div>
+        <div className="create-mock-attributes">
+          <div>
+            <p>Questions: 25-30</p>
+            <p>Approx time: 2 hours</p>
           </div>
           <div>
             <Button variant="contained">Start</Button>
